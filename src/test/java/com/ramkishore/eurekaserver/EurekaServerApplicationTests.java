07ef8d887df7b9934e711cf5702704d010fe8d0c
RAMKISHORE DYAVANPALLY <48268023+ramkishore417@users.noreply.github.com>
2023-03-10 12:16:02 +0530

    initial commit

---
+++ src/test/java/com/ramkishore/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramkishore.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
